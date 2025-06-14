211ff6713c52ce1ca24cc02fab8f58524b5c9d8c
JonTepes <jon@tepes.info>
2025-06-15 01:04:34 +0200

    Add Node.js server for digital twin, MJPG stream, and MQTT data

---
+++ server.js
@@ -0,0 +1,62 @@
+// TODO: Remember to install dependencies: npm install express socket.io mqtt node-fetch@2
+const express = require('express');
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const mqtt = require('mqtt');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+const port = 3000;
+
+// Serve static files from the digital_double directory
+app.use(express.static(path.join(__dirname, 'digital_double')));
+
+// MJPG stream proxy
+app.get('/camera_stream', async (req, res) => {
+  // TODO: Change to http://iotlinija.ddns.net:8081/?action=stream if needed
+  const streamUrl = 'http://localhost:8081/?action=stream';
+  try {
+    const response = await fetch(streamUrl);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch stream: ${response.status} ${response.statusText}`);
+    }
+
+    res.setHeader('Content-Type', 'multipart/x-mixed-replace; boundary=frame');
+    response.body.pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching camera stream');
+  }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'digital_double', 'index.html'));
+});
+
+const mqttClient = mqtt.connect('mqtt://192.168.1.150:1883');
+
+mqttClient.on('connect', () => {
+  console.log('Connected to MQTT broker');
+  mqttClient.subscribe('factory/data');
+});
+
+mqttClient.on('message', (topic, message) => {
+  console.log(`Received message on topic ${topic}: ${message.toString()}`);
+  io.emit('mqtt_message', { topic: topic, message: message.toString() });
+});
+
+io.on("connection", (socket) => {
+  console.log('a user connected');
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
